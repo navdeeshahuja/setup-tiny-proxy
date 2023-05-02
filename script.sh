37b582c1024ed889959dcf5d30ac10cba5c92535
Navdeesh Ahuja <navdeeshahuja@gmail.com>
2023-05-02 14:37:45 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get -y install tinyproxy
 
 sudo mv /etc/tinyproxy/tinyproxy.conf /etc/tinyproxy/tinyproxy.backup.conf
-sudo chmod 777 /etc/tinyproxy/tinyproxy.conf
-sudo curl https://raw.githubusercontent.com/navdeeshahuja/setup-tiny-proxy/main/tinyproxy.conf > /etc/tinyproxy/tinyproxy.conf
+curl https://raw.githubusercontent.com/navdeeshahuja/setup-tiny-proxy/main/tinyproxy.conf > tinyproxy.conf
+sudo mv tinyproxy.conf /etc/tinyproxy/tinyproxy.conf
 sudo echo 'BasicAuth username password' >> /etc/tinyproxy/tinyproxy.conf
 
 sudo service tinyproxy restart
